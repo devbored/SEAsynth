9fd898c13b3d4aa9c0684f9a6da2b3481640006e
Austin Annestrand <austin.annestrand@gmail.com>
2022-05-19 19:43:25 -0500

    Format changes for mnist train script

--- scripts/train_mnist.py
+++ scripts/train_mnist.py
@@ -6,19 +6,20 @@ from tensorflow.compat.v1 import keras
 
 def train():
     # Load the data
-    minst = keras.datasets.mnist
-    (train_images, train_labels), (test_images, test_labels) = minst.load_data()
+    mnist = keras.datasets.mnist
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
     # Normalize the input image so that each pixel value is between 0 to 1.
-    train_images = train_images / 255.0
-    test_images = test_images / 255.0
+    train_images    = train_images / 255.0
+    test_images     = test_images  / 255.0
 
-    # Create custom PynqNet model
+    # Create custom model
     model = keras.Sequential([
         keras.layers.InputLayer(input_shape=(28,28)),
         keras.layers.Reshape(target_shape=(28,28,1)),
-        keras.layers.Conv2D(12, kernel_size=(3,3),
-            strides=(1,1), activation='relu', input_shape=(28,28,1), use_bias=False),
+        keras.layers.Conv2D(
+            12, kernel_size=(3,3), strides=(1,1), activation='relu', input_shape=(28,28,1), use_bias=False
+        ),
         keras.layers.MaxPooling2D(pool_size=(2,2)),
         keras.layers.MaxPooling2D(pool_size=(2,2)),
         keras.layers.Flatten(),
@@ -32,12 +33,13 @@ def train():
         metrics=["accuracy"]
     )
     model.fit(train_images, train_labels, epochs=10)
-    print("[Info]: PynqNet successfully trained.")
+    print(f"\n[Info]: Successfully trained.")
 
     # Test accuracy
     score = model.evaluate(test_images, test_labels, verbose=0)
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
+    print("==============================")
+    print(f"Test loss     : {score[0]}")
+    print(f"Test accuracy : {score[1]}")
 
     # Extract trained layer values
     # (layer1 = 'conv2d', layer5 = 'dense')
@@ -49,7 +51,7 @@ def train():
     weights_fc1_dir = "build/raw-mnist-weights-float32/fc"
     Path(weights_conv2d_dir).mkdir(parents=True, exist_ok=True)
     Path(weights_fc1_dir).mkdir(parents=True, exist_ok=True)
-    print("[Info]: Converting trained weight data to raw hexadecimal float32 values...")
+    print("\n[Info]: Converting trained weight data to raw hexadecimal float32 values...")
 
     # Convert conv2d layer values
     cell_idx = 0
@@ -57,7 +59,6 @@ def train():
         for col in row:
             # Open weight-file-[cell-idx].mem
             cell_file = open(Path(weights_conv2d_dir + "/cell-weight-" + str(cell_idx) + ".mem"), 'w')
-
             # Get weights for the current cell idx
             for kernel_px in col[0]:
                 f32_val = float(kernel_px)
@@ -66,7 +67,6 @@ def train():
                 byte_lst.reverse()
                 raw_f32_val = "".join(byte_lst) + "\n"
                 cell_file.write(raw_f32_val)
-
             # Close the cell's weight file; move to next cell
             cell_file.close()
             cell_idx += 1
